fc681d4822834b44a1ecbbe158ec494d59ee1d18
Tom Marsh <ts.marsh@gmail.com>
2012-09-30 21:05:12 -0400

    Compressing the chapter together

--- src/koans/playthings.clj
+++ src/koans/playthings.clj
@@ -33,4 +33,50 @@
   			(== x true) 
   			(== true q))) 
   	__)
+
+  	"If x is fresh, the (v x) succeeds and associates x with v"
+	(= (run* [q] 
+		(fresh [x] 
+			(==  x true)
+			(== true q)
+			))
+		__)
+
+		"_.0 is a variable representing a fresh variable."
+	(= (run* [x] s#) __)
+
+	"What is the value of x?"
+	(= (run* [x] 
+			(let [x false]
+				(fresh [x] 
+					(== true x))))
+	__)
+
+	"It is possible to return more than one fresh variable."
+	(= (run* [r]
+		(fresh [x y]
+			(== (cons x (cons y '())) r)))
+	__)
+
+	"The name of the variable does not affect freshness."
+	(= (run* [s]
+		(fresh [t u]
+			(== (cons t (cons u '())) s)))
+	__)
+
+	"The freshest fresh wins"
+	(= (run* [r] 
+		(fresh [x] 
+			(let [y x]
+				(fresh [x]
+					(== (cons y (cons x (cons y '()))) r)))))
+	__)
+
+	"Freshness is applied in the order they are reified, not declared"
+	(= (run* [r] 
+		(fresh [x] 
+			(let [y x]
+				(fresh [x]
+					(== (cons x (cons y (cons x '()))) r)))))
+	__)
 )
