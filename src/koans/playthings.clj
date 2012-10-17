5cb55cbd2e00533dc6684e4d34bfad02aa15ac8f
Tom Marsh <ts.marsh@gmail.com>
2012-10-17 16:18:33 -0400

    Adding exercise numbers from the book so that I can keep track of where I am.

--- src/koans/playthings.clj
+++ src/koans/playthings.clj
@@ -1,40 +1,40 @@
 (meditations
-  "run* always returns a list, even when it fails (u#)"
+  "1.10 run* always returns a list, even when it fails (u#)"
   (= (run* [q] u#) 
   	__)
 
-  "A successful run can associate values to q"
+  "1.11 A successful run can associate values to q"
   (= (run* [q] 
   	(== true q)) __)
 
-  "Failing runs do not associate values."
+  "1.12 Failing runs do not associate values."
   (= (run* [q]
   	u#
   	(== true q)) __)
 
-  "So what is the value here?"
+  "1.13 So what is the value here?"
   (= (run* [q]
   	s#
   	(== true q)) __)
 
-  "You can associate any value."
+  "1.15 You can associate any value."
   (= (run* [r]
   	s#
   	(== 'corn r)) __)
 
-  "You can assign variabels within a run."
+  "1.22 You can assign variabels within a run."
   (= (run* [x] 
   		(let [x false] (== true x))) 
   	__)
 
-  "You can create 'fresh' variables that have no associations."
+  "1.23 You can create 'fresh' variables that have no associations."
   (= (run* [q]
   		(fresh [x] 
   			(== x true) 
   			(== true q))) 
   	__)
 
-  	"If x is fresh, the (v x) succeeds and associates x with v"
+  "1.26 If x is fresh, the (v x) succeeds and associates x with v"
 	(= (run* [q] 
 		(fresh [x] 
 			(==  x true)
@@ -42,29 +42,29 @@
 			))
 		__)
 
-		"_.0 is a variable representing a fresh variable."
+	"1.28 _.0 is a variable representing a fresh variable."
 	(= (run* [x] s#) __)
 
-	"What is the value of x?"
+	"1.29 What is the value of x?"
 	(= (run* [x] 
 			(let [x false]
 				(fresh [x] 
 					(== true x))))
 	__)
 
-	"It is possible to return more than one fresh variable."
+	"1.30 It is possible to return more than one fresh variable."
 	(= (run* [r]
 		(fresh [x y]
 			(== (cons x (cons y '())) r)))
 	__)
 
-	"The name of the variable does not affect freshness."
+	"1.31 The name of the variable does not affect freshness."
 	(= (run* [s]
 		(fresh [t u]
 			(== (cons t (cons u '())) s)))
 	__)
 
-	"The freshest fresh wins"
+	"1.32 The freshest fresh wins"
 	(= (run* [r] 
 		(fresh [x] 
 			(let [y x]
@@ -72,7 +72,7 @@
 					(== (cons y (cons x (cons y '()))) r)))))
 	__)
 
-	"Freshness is applied in the order they are reified, not declared"
+	"1.33 Freshness is applied in the order they are reified, not declared"
 	(= (run* [r] 
 		(fresh [x] 
 			(let [y x]
